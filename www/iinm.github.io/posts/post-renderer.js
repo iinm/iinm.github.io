3797babcb506688c59f17371722e08738e45abdd
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2024-05-25 22:30:07 +0900

    fix: tsconfig

--- www/iinm.github.io/posts/post-renderer.js
+++ www/iinm.github.io/posts/post-renderer.js
@@ -8,50 +8,56 @@ import {
 import { writeVirtualDom } from "../lib/dom.js";
 import { MetaContents, Post } from "./components/post.js";
 
-const basePath = location.pathname.split("/").slice(0, -1).join("/"); // "/posts"
-const pagePath = location.search
-  .slice(1) // remove '?'
-  .split("&")
-  .map((pair) => pair.split("="))
-  .find(([key]) => key === "path")?.[1]; // yyyy-MM-dd--<title>.html
-const pageName = pagePath?.split("/").slice(-1)[0].replace(".html", "");
-const contentPath = `${basePath}/source/${pageName}.md`;
+async function render() {
+  const basePath = location.pathname.split("/").slice(0, -1).join("/"); // "/posts"
+  const pagePath = location.search
+    .slice(1) // remove '?'
+    .split("&")
+    .map((pair) => pair.split("="))
+    .find(([key]) => key === "path")?.[1]; // yyyy-MM-dd--<title>.html
+  const pageName = pagePath?.split("/").slice(-1)[0].replace(".html", "");
+  const contentPath = `${basePath}/source/${pageName}.md`;
 
-// fetch content
-const contentResponse = await fetch(contentPath);
-if (!contentResponse.ok) {
-  throw new Error("Failed to fetch content");
+  // fetch content
+  const contentResponse = await fetch(contentPath);
+  if (!contentResponse.ok) {
+    throw new Error("Failed to fetch content");
+  }
+  const markdownContent = await contentResponse.text();
+
+  // parse content
+  const markdownBlocks = parseMarkdown(markdownContent);
+
+  // metadata
+  const date = pagePath?.match(/^\d{4}-\d{2}-\d{2}/)?.[0] || "";
+  const pageTitle = firstHeadingAsString(markdownBlocks) || "";
+  const description = firstParagraphAsString(markdownBlocks) || "";
+  const metadata = {
+    title: pageTitle,
+    date,
+    description: description,
+    ogp: {
+      "og:url": `https://iinm.github.io/posts/${pagePath}`,
+      "og:type": "article",
+      "og:title": pageTitle,
+      "og:description": description,
+      "og:image": "https://avatars.githubusercontent.com/u/8685693",
+      "og:site_name": "iinm.github.io",
+    },
+  };
+
+  // render
+  writeVirtualDom(MetaContents({ metadata }), document.head);
+  writeVirtualDom(
+    Post({ markdownBlocks, metadata }),
+    /** @type {HTMLElement} */ (document.querySelector(".post"))
+  );
+
+  /** @type {HTMLElement} */ (
+    document.querySelector("#renderer-script")
+  ).remove();
 }
-const markdownContent = await contentResponse.text();
 
-// parse content
-const markdownBlocks = parseMarkdown(markdownContent);
-
-// metadata
-const date = pagePath?.match(/^\d{4}-\d{2}-\d{2}/)?.[0] || "";
-const pageTitle = firstHeadingAsString(markdownBlocks) || "";
-const description = firstParagraphAsString(markdownBlocks) || "";
-const metadata = {
-  title: pageTitle,
-  date,
-  description: description,
-  ogp: {
-    "og:url": `https://iinm.github.io/posts/${pagePath}`,
-    "og:type": "article",
-    "og:title": pageTitle,
-    "og:description": description,
-    "og:image": "https://avatars.githubusercontent.com/u/8685693",
-    "og:site_name": "iinm.github.io",
-  },
-};
-
-// render
-writeVirtualDom(MetaContents({ metadata }), document.head);
-writeVirtualDom(
-  Post({ markdownBlocks, metadata }),
-  /** @type {HTMLElement} */ (document.querySelector(".post"))
-);
-
-/** @type {HTMLElement} */ (
-  document.querySelector("#renderer-script")
-).remove();
+render().catch((err) => {
+  console.error(err);
+});
